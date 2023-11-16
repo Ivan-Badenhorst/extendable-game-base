a2f1f3b367d479d7d8e9eee18fc509c7eeada723
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-11-16 18:26:01 +0100

    memory management included

--- gamecontroller.cpp
+++ gamecontroller.cpp
@@ -27,17 +27,14 @@ void GameController::startGame(MainWindow & mw)
     std::cout << "made factory" << std::endl;
     auto level = easyLevelFactory.createWorld(mw);
     EasyLevel* easyLevel = static_cast<EasyLevel*>(level);
-    //EasyLevel* easy = std::static_pointer_cast<EasyLevel>(es);
-
-    //auto easyLevel = std::unique_ptr<EasyLevel>(dynamic_cast<EasyLevel*>(level.release()));
-    //auto easyLevel = std::make_unique<EasyLevel>(*level);
-    //auto easyLevel = std::unique_ptr<EasyLevel>(static_cast<EasyLevel*>(level.release()));
 
     std::cout << "made level" << std::endl;
 
     auto tileController = easyLevel->getTileController();
     tileController->update();
     std::cout << "called update" << std::endl;
+
+    delete easyLevel;
 
 }
 
