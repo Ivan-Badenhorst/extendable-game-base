44f7f8ea1859a6a0d2018fec4249eb2f57df2e65
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-11-18 16:57:29 +0100

    header setup for healthpackcontroller.h

--- HealthPackH/healthpackcontroller.h
+++ HealthPackH/healthpackcontroller.h
@@ -1,11 +1,19 @@
 #ifndef HEALTHPACKCONTROLLER_H
 #define HEALTHPACKCONTROLLER_H
 
+#include "HealthPackH/healthpackmodel.h"
+#include "HealthPackH/healthpackview.h"
 
 class HealthPackController
 {
 public:
-    HealthPackController();
+    HealthPackController(std::shared_ptr<HealthPackView> hpv, std::shared_ptr<HealthPackModel> hpm);
+    //add an update method!! See model for more details
+
+private:
+    std::shared_ptr<HealthPackView> hpView;
+    std::shared_ptr<HealthPackModel> hpModel;
+
 };
 
 #endif // HEALTHPACKCONTROLLER_H
