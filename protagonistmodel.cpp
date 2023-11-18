9a09c8d796142570beda08796127aaad673c885a
Sudarshan Iyengar <sudarshan.iyengar@student.kuleuven.be>
2023-11-18 21:07:55 +0100

    ProtagonistModel should be done. Created a struct for protagonist data

--- protagonistmodel.cpp
+++ protagonistmodel.cpp
@@ -1,6 +1,16 @@
 #include "protagonistmodel.h"
 
-ProtagonistModel::ProtagonistModel()
+ProtagonistModel::ProtagonistModel(std::unique_ptr<Protagonist> prot)
 {
-
+    protagonistData.x = prot->getXPos();
+    protagonistData.y = prot->getYPos();
+    protagonistData.health = prot->getHealth();
+    protagonistData.energy = prot->getEnergy();
 }
+
+ProtagonistData ProtagonistModel::getProtagonistData() const
+{
+    return protagonistData;
+}
+
+
--- protagonistmodel.h
+++ protagonistmodel.h
@@ -3,10 +3,29 @@
 
 #include "world.h"
 
+
+struct ProtagonistData {
+    int x;
+    int y;
+    float health;
+    float energy;
+};
+
 class ProtagonistModel: public World
 {
 public:
-    ProtagonistModel();
+    ProtagonistModel(std::unique_ptr<Protagonist> prot);
+    ProtagonistData getProtagonistData() const;
+    // Methods to interact with the stored protagonist data
+    int getPositionX() const { return protagonistData.x; }
+    int getPositionY() const { return protagonistData.y; }
+    float getHealth() const { return protagonistData.health; }
+    void setHealth(float value) { protagonistData.health = value; }
+    float getEnergy() const { return protagonistData.energy; }
+    void setEnergy(float value) { protagonistData.energy = value; }
+    // Add more methods as and when needed (also question. should i make them all inline?)
+private:
+    ProtagonistData protagonistData; // Struct to store protagonist data
 };
 
 #endif // PROTAGONISTMODEL_H
