ca1c734f719c75e84b196a2a6418f02eba679929
Sudarshan Iyengar <sudarshan.iyengar@student.kuleuven.be>
2023-11-22 14:36:24 +0100

    Modified protagonist model to remove the struct and just use attributes.

--- protagonistmodel.cpp
+++ protagonistmodel.cpp
@@ -1,16 +1,25 @@
 #include "protagonistmodel.h"
+#include <iostream>
 
 ProtagonistModel::ProtagonistModel(std::unique_ptr<Protagonist> prot)
+    : protagonist(std::move(prot))
 {
-    protagonistData.x = prot->getXPos();
-    protagonistData.y = prot->getYPos();
-    protagonistData.health = prot->getHealth();
-    protagonistData.energy = prot->getEnergy();
+//    protagonistData.x = prot->getXPos();
+//    protagonistData.y = prot->getYPos();
+//    protagonistData.health = prot->getHealth();
+    //    protagonistData.energy = prot->getEnergy();
 }
 
-ProtagonistData ProtagonistModel::getProtagonistData() const
-{
-    return protagonistData;
-}
+//std::unique_ptr<Protagonist> ProtagonistModel::getProtagonist() const
+//{
+//    return protagonist;
+//}
+
+
+
+//ProtagonistData ProtagonistModel::getProtagonistData() const
+//{
+//    return protagonistData;
+//}
 
 
--- protagonistmodel.h
+++ protagonistmodel.h
@@ -4,28 +4,42 @@
 #include "world.h"
 
 
-struct ProtagonistData {
-    int x;
-    int y;
-    float health;
-    float energy;
-};
+//struct ProtagonistData {
+//    int x;
+//    int y;
+//    float health;
+//    float energy;
+//};
 
 class ProtagonistModel: public World
 {
 public:
     ProtagonistModel(std::unique_ptr<Protagonist> prot);
-    ProtagonistData getProtagonistData() const;
+//    ProtagonistModel() = default;
     // Methods to interact with the stored protagonist data
-    int getPositionX() const { return protagonistData.x; }
-    int getPositionY() const { return protagonistData.y; }
-    float getHealth() const { return protagonistData.health; }
-    void setHealth(float value) { protagonistData.health = value; }
-    float getEnergy() const { return protagonistData.energy; }
-    void setEnergy(float value) { protagonistData.energy = value; }
+//     ProtagonistData getProtagonistData() const;
+//    int getPositionX() const { return protagonistData.x; }
+//    int getPositionY() const { return protagonistData.y; }
+//    float getHealth() const { return protagonistData.health; }
+//    void setHealth(float value) { protagonistData.health = value; }
+//    float getEnergy() const { return protagonistData.energy; }
+//    void setEnergy(float value) { protagonistData.energy = value; }
+
+    int getPositionX() const { return protagonist->getXPos(); }
+    int getPositionY() const { return protagonist->getYPos(); }
+    float getHealth() const { return protagonist->getHealth(); }
+    void setHealth(float value) { protagonist->setHealth(value); }
+    float getEnergy() const { return protagonist->getEnergy(); }
+    void setEnergy(float value) { protagonist->setEnergy(value); }
     // Add more methods as and when needed (also question. should i make them all inline?)
+
+    // add a method that moves it
+
+//    std::shared_ptr<Protagonist> getProtagonist() const;
+
 private:
-    ProtagonistData protagonistData; // Struct to store protagonist data
+//    ProtagonistData protagonistData; // Struct to store protagonist data
+    std::unique_ptr<Protagonist> protagonist; // Unique pointer to hold the Protagonist
 };
 
 #endif // PROTAGONISTMODEL_H
