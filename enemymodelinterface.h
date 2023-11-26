80bb90adfb5d330db36e0a4533e7c3528f851320
Mahmoud Chairi <mahmoud.chairi@student.kuleuven.be>
2023-11-26 09:46:22 +0100

    Added methods to the model interface

--- enemymodelinterface.h
+++ enemymodelinterface.h
@@ -1,4 +1,17 @@
 #ifndef ENEMYMODELINTERFACE_H
 #define ENEMYMODELINTERFACE_H
 
+#include "world.h"
+#include <vector>
+
+/* As we need more methods to control the game, we will add them here and all
+    models that implement this interface will have to implement the new methods
+*/
+class EnemyModelInterface
+{
+public:
+    virtual std::vector<Enemy> getEnemies() const = 0;
+    virtual bool containsEnemy(int row, int col) = 0;
+};
+
 #endif // ENEMYMODELINTERFACE_H
