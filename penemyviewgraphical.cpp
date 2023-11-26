12c91ee3c6138c4ee90cc037944be776026c3778
Mahmoud Chairi <mahmoud.chairi@student.kuleuven.be>
2023-11-26 12:17:46 +0100

    Implemented the view for PEnemy

--- penemyviewgraphical.cpp
+++ penemyviewgraphical.cpp
@@ -1,6 +1,29 @@
 #include "penemyviewgraphical.h"
+#include <iostream>
+#include <QPixmap>
+#include <QGraphicsPixmapItem>
 
-PEnemyViewGraphical::PEnemyViewGraphical()
+PEnemyViewGraphical::PEnemyViewGraphical(MainWindow& mw, std::shared_ptr<PEnemyModel> pem)
+:mainWindow(mw), penemyModel(pem)
 {
 
+}
+
+void PEnemyViewGraphical::update()
+{
+    penemyDisplays.clear();
+
+    auto enemyIcon = QPixmap(":/PEnemy_alive");
+
+    for(auto& penemy: penemyModel->getEnemies()){
+        displayEnemy(std::make_shared<QGraphicsPixmapItem>(enemyIcon), penemy.get()->getXPos(), penemy.get()->getYPos());
+    }
+}
+
+void PEnemyViewGraphical::displayEnemy(std::shared_ptr<QGraphicsPixmapItem> icon, int x, int y)
+{
+    icon->setPos(x*tileDim, y*tileDim);
+    icon->setZValue(zValue);
+    mainWindow.getScene()->addItem(icon.get());
+    penemyDisplays.push_back(icon);
 }
--- penemyviewgraphical.h
+++ penemyviewgraphical.h
@@ -1,11 +1,26 @@
 #ifndef PENEMYVIEWGRAPHICAL_H
 #define PENEMYVIEWGRAPHICAL_H
+#include <memory>
+#include "enemyviewinterface.h"
+#include "penemymodel.h"
+#include "mainwindow.h"
 
-
-class PEnemyViewGraphical
+class PEnemyViewGraphical: public EnemyViewInterface
 {
 public:
-    PEnemyViewGraphical();
+    PEnemyViewGraphical(MainWindow& mw, std::shared_ptr<PEnemyModel> pem);
+    void update() override;
+    //void update(int row, int col, bool used = true) override;
+
+private:
+    MainWindow& mainWindow;
+    std::shared_ptr<PEnemyModel> penemyModel;
+
+    float zValue {1};
+    int tileDim {50};
+
+    std::vector<std::shared_ptr<QGraphicsPixmapItem>> penemyDisplays;
+    void displayEnemy(std::shared_ptr<QGraphicsPixmapItem> icon, int x, int y);
 };
 
 #endif // PENEMYVIEWGRAPHICAL_H
