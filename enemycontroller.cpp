2eafc5d4ab3d0020f6ba4008316d307a94d03af4
Mahmoud Chairi <mahmoud.chairi@student.kuleuven.be>
2023-11-26 10:21:15 +0100

    Added models to the enemy controller

--- enemycontroller.cpp
+++ enemycontroller.cpp
@@ -2,5 +2,11 @@
 
 EnemyController::EnemyController()
 {
+    // Models are added in the factory using the addEnemyModel method
+}
 
+void EnemyController::addEnemyModel(std::shared_ptr<EnemyModelInterface> em)
+{
+    // Add the model to the vector
+    enemyModels.push_back(em);
 }
--- enemycontroller.h
+++ enemycontroller.h
@@ -1,11 +1,21 @@
 #ifndef ENEMYCONTROLLER_H
 #define ENEMYCONTROLLER_H
+#include "enemymodel.h"
+//#include "enemyview.h"
+#include <memory>
 
 
 class EnemyController
 {
 public:
     EnemyController();
+    //void refreshAll();
+    //bool update(int row, int col);//row and col are tile numbers
+    void addEnemyModel(std::shared_ptr<EnemyModelInterface> em);
+
+private:
+    std::vector<std::shared_ptr<EnemyModelInterface>> enemyModels;
+    
 };
 
 #endif // ENEMYCONTROLLER_H
