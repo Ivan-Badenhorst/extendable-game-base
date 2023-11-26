05bd6efd7b97354d29a829cb8811a78f7fdad7e7
Mahmoud Chairi <mahmoud.chairi@student.kuleuven.be>
2023-11-26 10:14:29 +0100

    Implemented model for PEnemy

--- penemymodel.cpp
+++ penemymodel.cpp
@@ -2,5 +2,51 @@
 
 PEnemyModel::PEnemyModel()
 {
+    // The model is empty when created. Enemies are added using the addEnemy method from the factory.
+}
 
+void PEnemyModel::addEnemy(std::shared_ptr<Enemy> enemy)
+{
+    // Get the x and y position of the enemy
+    int x = enemy->getXPos();
+    int y = enemy->getYPos();
+
+    // Create a pair with the x and y position as the key
+    std::pair<int, int> key = std::make_pair(x, y);
+
+    // Add the enemy to the enemyMap with the key
+    enemyMap[key] = enemy;
+}
+
+std::vector<std::shared_ptr<Enemy>> PEnemyModel::getEnemies() const
+{
+    // Create a vector to hold the enemies
+    std::vector<std::shared_ptr<Enemy>> enemies;
+
+    // Iterate through the enemyMap and add the enemies to the vector
+    for (auto const& x : enemyMap)
+    {
+        enemies.push_back(x.second);
+    }
+
+    // Return the vector
+    return enemies;
+}
+
+bool PEnemyModel::containsEnemy(int row, int col)
+{
+    // Create a pair with the x and y position as the key
+    std::pair<int, int> key = std::make_pair(row, col);
+
+    // Check if the enemyMap contains the key
+    if (enemyMap.count(key) > 0)
+    {
+        // Return true if the enemyMap contains the key
+        return true;
+    }
+    else
+    {
+        // Return false if the enemyMap does not contain the key
+        return false;
+    }
 }
--- penemymodel.h
+++ penemymodel.h
@@ -1,11 +1,19 @@
 #ifndef PENEMYMODEL_H
 #define PENEMYMODEL_H
+#include <memory>
+#include <map>
+#include "enemymodelinterface.h"
 
-
-class PEnemyModel
+class PEnemyModel: public EnemyModelInterface
 {
 public:
     PEnemyModel();
+    void addEnemy(std::shared_ptr<Enemy> enemy) override;
+    std::vector<std::shared_ptr<Enemy>> getEnemies() const override;
+    bool containsEnemy(int row, int col) override;
+
+private:
+    std::map<std::pair<int, int>, std::shared_ptr<Enemy>> enemyMap; // Map to link x and y to shared pointer of enemy object
 };
 
 #endif // PENEMYMODEL_H
