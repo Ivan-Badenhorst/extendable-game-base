5e724fcd0fe1897960b25e0550b5b4e27e86f7a0
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-11-30 21:09:58 +0100

    update implemented with curson -> but no change seen

--- healthpackviewtext.cpp
+++ healthpackviewtext.cpp
@@ -10,13 +10,55 @@ HealthPackViewText::HealthPackViewText()
 
 void HealthPackViewText::update()
 {
-//implement
+    for(auto& hp: hpModel->getHealthPacks()){
+
+        if(hp[2]>0){//unused pack
+            update(hp[0], hp[1], false);
+        }
+        else
+        {
+            update(hp[0], hp[1]);
+        }
+
+    }
 }
 
 void HealthPackViewText::update(int row, int col, bool used)
 {
 
 
+
+    int moveDown = 1 + 3*row;   // Number of lines to move down
+    int moveRight = 2 + 5*col;  // Number of characters to move to the right
+
+    auto cursor = textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor);
+    cursor.insertText("h");
+    cursor.insertText("h");
+
+
+    ///REPLACE THE FOR LOOP!!! FUNCTION CAN TAKE 3 PARAMETERS HEHE
+    // Move the cursor down by 'moveDown' lines
+    for (int i = 0; i < moveDown; ++i) {
+        cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor);
+    }
+
+    // Move the cursor to the right by 'moveRight' characters
+    for (int i = 0; i < moveRight; ++i) {
+        cursor.movePosition(QTextCursor::Right, QTextCursor::MoveAnchor);
+    }
+
+    // Set the new cursor position
+    textEdit->setTextCursor(cursor);
+
+    // Replace the character at the new cursor position
+    if (cursor.hasSelection()) {
+        // If there is a selection, replace the selected text
+        cursor.removeSelectedText();
+    }
+
+    if(used) {cursor.insertText("h");}
+    else {cursor.insertText("H");}
 
 }
 
