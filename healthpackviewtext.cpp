46909a438f70225e02006bf809308abcf7142ece
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-11-30 21:50:25 +0100

    cleaned update function

--- healthpackviewtext.cpp
+++ healthpackviewtext.cpp
@@ -27,46 +27,19 @@ void HealthPackViewText::update()
 void HealthPackViewText::update(int row, int col, bool used)
 {
 
-
-
     int moveDown = 1 + 2*row;   // Number of lines to move down
     int moveRight = 2 + 4*col;  // Number of characters to move to the right
 
-    textEdit->setReadOnly(false);
     auto cursor = textEdit->textCursor();
     cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor);
     cursor.clearSelection();
 
+    cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor, moveDown);
+    cursor.movePosition(QTextCursor::Right, QTextCursor::MoveAnchor, moveRight);
 
-
-
-    ///REPLACE THE FOR LOOP!!! FUNCTION CAN TAKE 3 PARAMETERS HEHE
-    // Move the cursor down by 'moveDown' lines
-    for (int i = 0; i < moveDown; ++i) {
-        cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor);
-    }
-
-    // Move the cursor to the right by 'moveRight' characters
-    for (int i = 0; i < moveRight; ++i) {
-        cursor.movePosition(QTextCursor::Right, QTextCursor::MoveAnchor);
-    }
-
-    // Set the new cursor position
-    textEdit->setTextCursor(cursor);
-
-    // Replace the character at the new cursor position
     cursor.deleteChar();
-
-
     if(used) {cursor.insertText("h");}
     else {cursor.insertText("H");}
-    textEdit->repaint();
-
-    textEdit->setTextCursor(cursor);
-    textEdit->viewport()->update();
-    QApplication::processEvents();
-    textEdit->setReadOnly(true);
-
 
 }
 
