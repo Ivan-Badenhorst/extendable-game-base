f50314760d9f11d4a3146128d054fc173c929745
Sudarshan Iyengar <sudarshan.iyengar@student.kuleuven.be>
2023-12-01 01:10:41 +0100

    Implemented basic working functionality to display the protagonist. Bugs galore

--- protagonistviewtext.cpp
+++ protagonistviewtext.cpp
@@ -8,22 +8,39 @@ ProtagonistViewText::ProtagonistViewText()
 
 void ProtagonistViewText::update()
 {
+    auto xPos = protModel->getPositionX();
+    auto yPos = protModel->getPositionY();
+
+
+    update(xPos,yPos);
 
 }
 
 void ProtagonistViewText::update(int row, int col)
 {
+    int moveDown = 1 + 2*row;
+    int moveRight = 2 + 4*col;
+
+    auto cursor = textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor);
+    cursor.clearSelection();
+
+    cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor, moveDown);
+    cursor.movePosition(QTextCursor::Right, QTextCursor::MoveAnchor, moveRight);
+
+    cursor.deleteChar();
+    cursor.insertText("P");
 
 }
 
 void ProtagonistViewText::updateHealth()
 {
-
+    //yet to implement that
 }
 
 void ProtagonistViewText::clearView()
 {
-
+    textEdit.reset();
 }
 
 void ProtagonistViewText::setProtModel(const std::shared_ptr<ProtagonistModel> &newProtModel)
