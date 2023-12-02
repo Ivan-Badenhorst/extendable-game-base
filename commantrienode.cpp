2496f4034aab6fb1749b50912d5f95441d62fe41
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-12-02 21:34:23 +0100

    Insert function implemented

--- commantrienode.cpp
+++ commantrienode.cpp
@@ -4,3 +4,43 @@ CommandTrieNode::CommandTrieNode()
 {
 
 }
+
+void CommandTrieNode::insert(std::string &command)
+{
+
+    std::shared_ptr<CommandTrieNode> currentNode = std::make_shared<CommandTrieNode>(*this);
+
+    for (int i = 0; i < command.length(); i++) {
+        char letter = command[i];
+
+        /* make a new node if there is no path */
+        if (currentNode->noChildNode(letter))
+            currentNode->insertChildNode(letter);
+            //currentNode->map[letter] = getNewTrieNode();
+
+        currentNode = currentNode->getChildNode(letter);
+    }
+
+    currentNode->setEndOfCommand(true);
+
+}
+
+bool CommandTrieNode::noChildNode(const char &letter) const
+{
+    return (childNodes.find(letter) == childNodes.end());
+}
+
+void CommandTrieNode::insertChildNode(const char &letter)
+{
+    childNodes[letter] = std::make_shared<CommandTrieNode>();
+}
+
+std::shared_ptr<CommandTrieNode> CommandTrieNode::getChildNode(const char &letter) const
+{
+    return childNodes.find(letter)->second;
+}
+
+void CommandTrieNode::setEndOfCommand(const bool &val)
+{
+    isEndOfCommand = val;
+}
--- commantrienode.h
+++ commantrienode.h
@@ -2,13 +2,23 @@
 #define COMMANTRIENODE_H
 
 
+#include <unordered_map>
+#include <memory>
+#include <string>
+
 class CommandTrieNode
 {
 public:
     CommandTrieNode();
+    void insert(std::string& command);
+    bool noChildNode(const char& letter) const;
+    void insertChildNode(const char& letter);
+    std::shared_ptr<CommandTrieNode> getChildNode(const char& letter) const;
+    void setEndOfCommand(const bool& val);
 
 private:
     bool isEndOfCommand {false};
+    std::unordered_map<char, std::shared_ptr<CommandTrieNode>> childNodes;
 
 };
 
