206ac78fad2592da168312e37d56db447781b966
Mahmoud Chairi <mahmoud.chairi@student.kuleuven.be>
2023-12-06 10:38:33 +0100

    Added the enemy type as part of view interface

--- enemyviewinterface.h
+++ enemyviewinterface.h
@@ -8,11 +8,13 @@ class EnemyViewInterface
 public:
     virtual void update() = 0;
     //virtual void update(int row, int col,  bool used = true) = 0;
-
+    virtual std::string getEnemyType() const = 0;
+    virtual void setEnemyType(const std::string &value) = 0;
     void setScene(const std::shared_ptr<QGraphicsScene> &newScene);
 
 protected:
     std::shared_ptr<QGraphicsScene> scene;
+    std::string enemyType; // Protected string attribute for enemy type
 };
 
 inline void EnemyViewInterface::setScene(const std::shared_ptr<QGraphicsScene> &newScene)
