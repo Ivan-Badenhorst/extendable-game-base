2705f9567829d8b51e70f911146bda854dc0d803
Mahmoud Chairi <mahmoud.chairi@student.kuleuven.be>
2023-12-06 13:20:40 +0100

    Added enemy type

--- enemymodelinterface.h
+++ enemymodelinterface.h
@@ -15,6 +15,21 @@ public:
     virtual bool containsEnemy(int row, int col) = 0;
     virtual bool isDefeated(int row, int col) = 0;
     virtual void attackEnemy(int row, int col, int damage) = 0;
+    std::string getEnemyType() const;
+    void setEnemyType(const std::string &value);
+
+protected:
+    std::string enemyType; // Protected string attribute for enemy type
 };
+
+inline std::string EnemyModelInterface::getEnemyType() const
+{
+    return enemyType;
+}
+
+inline void EnemyModelInterface::setEnemyType(const std::string &value)
+{
+    enemyType = value;
+}
 
 #endif // ENEMYMODELINTERFACE_H
