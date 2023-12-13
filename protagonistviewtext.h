11fa1305573389544a274861bcc01237b7beefb2
Sudarshan Iyengar <sudarshan.iyengar@student.kuleuven.be>
2023-12-13 10:30:15 +0100

    Interface now contains updateEnergy method that is overriden in implementing classes

--- protagonistviewtext.h
+++ protagonistviewtext.h
@@ -14,16 +14,19 @@ public:
     void update() override;
     void update(int row, int col) override;
     void updateHealth() override;
+    void updateEnergy() override;
     void clearView() override;
     void setProtModel(const std::shared_ptr<ProtagonistModel> &newProtModel) override;
     void erasePreviousPosition();
     void setTextEdit(const std::shared_ptr<QPlainTextEdit> &newTextEdit);
     void setHealthLabels(std::shared_ptr<QLabel> healthValue);
+    void setEnergyLabels(std::shared_ptr<QLabel> energyValue);
 
 private:
     std::shared_ptr<ProtagonistModel> protModel;
     std::shared_ptr<QPlainTextEdit> textEdit;
     std::shared_ptr<QLabel> healthValueLabel;
+    std::shared_ptr<QLabel> energyValueLabel;
 
 };
 
