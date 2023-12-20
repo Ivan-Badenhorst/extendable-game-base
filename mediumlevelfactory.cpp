2e01b232035a5243d8af9cd021e336de1468c43c
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-12-20 21:47:49 +0100

    Refactored mediumlevelfactory.cpp

--- mediumlevelfactory.cpp
+++ mediumlevelfactory.cpp
@@ -26,39 +26,38 @@ std::shared_ptr<Level> MediumLevelFactory::createWorld()
     w.createWorld(":/worldmap.png", 100,100);
 
     //tile
-    TileModel tm;
-    tm.populateTileMap(w.getRows(), w.getCols(), w.getTiles());
-    TileController tc(std::make_shared<TileModel>(tm));
+    auto tm = std::make_shared<TileModel>();
+    tm->populateTileMap(w.getRows(), w.getCols(), w.getTiles());
+    auto tc = std::make_shared<TileController>(tm);
 
     //protagonist:
     std::unique_ptr<Protagonist> protagonistPtr = w.getProtagonist();
     auto pm = std::make_shared<ProtagonistModel>(std::move(protagonistPtr));
-    ProtagonistController pc(pm);
+    auto pc = std::make_shared<ProtagonistController>(pm);
 
     //healthPacks
-    HealthPackModel hpm(w.getHealthPacks(), 10);
-    HealthPackController hpc(std::make_shared<HealthPackModel>(hpm));
+    auto hpm = std::make_shared<HealthPackModel>(w.getHealthPacks(), 10);
+    auto hpc = std::make_shared<HealthPackController>(hpm);
 
-    //enemies
-    EnemyModel em;
-    PEnemyModel pem;
+    //enemies models
+    auto em = std::make_shared<EnemyModel>();
+    em->setEnemyType("Enemy");
+    auto pem = std::make_shared<PEnemyModel>();
+    pem->setEnemyType("PEnemy");
 
     for (auto& enemy : w.getEnemies()) {
         if (auto pEnemy = dynamic_cast<PEnemy*>(enemy.get())) {
-            pem.addEnemy(std::move(enemy));
+            pem->addEnemy(std::move(enemy));
         } else {
-            em.addEnemy(std::move(enemy));
+            em->addEnemy(std::move(enemy));
         }
     }
+
+    //enemy controller
     auto ec = std::make_shared<EnemyController>();
     ec->init();
-    ec->addEnemyModel(std::make_shared<EnemyModel>(em));
-    ec->addEnemyModel(std::make_shared<PEnemyModel>(pem));
+    ec->addEnemyModel(em);
+    ec->addEnemyModel(pem);
 
-    auto mediumLevel = std::make_shared<MediumLevel>( std::make_shared<TileController>(tc),
-                                                           std::make_shared<ProtagonistController>(pc),
-                                                           std::make_shared<HealthPackController>(hpc),
-                                                           ec);
-
-    return mediumLevel;
+    return std::make_shared<MediumLevel>(tc,  pc, hpc, ec);
 }
