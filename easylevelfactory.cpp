75ecd12c238067e874eead302ae0b289eaccecba
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-12-20 21:23:26 +0100

    easylevelfactory.cpp refactored

--- easylevelfactory.cpp
+++ easylevelfactory.cpp
@@ -3,15 +3,11 @@
 
 #include "protagonistcontroller.h"
 #include "protagonistmodel.h"
-
 #include "TileH/tilemodel.h"
 #include "TileH/tilecontroller.h"
-
 #include "easylevel.h"
 #include "HealthPackH/healthpackmodel.h"
 #include "HealthPackH/healthpackcontroller.h"
-
-
 #include "enemymodel.h"
 #include "penemymodel.h"
 #include "enemycontroller.h"
@@ -28,56 +24,39 @@ std::shared_ptr<Level> EasyLevelFactory::createWorld()
     w.createWorld(":/worldmap.png", 100,10);
 
     //tile
-    TileModel tm;
-    tm.populateTileMap(w.getRows(), w.getCols(), w.getTiles());
-    TileController tc(std::make_shared<TileModel>(tm));
+    auto tm = std::make_shared<TileModel>();
+    tm->populateTileMap(w.getRows(), w.getCols(), w.getTiles());
+    auto tc = std::make_shared<TileController>(tm);
 
     //protagonist:
     std::unique_ptr<Protagonist> protagonistPtr = w.getProtagonist();
     auto pm = std::make_shared<ProtagonistModel>(std::move(protagonistPtr));
-    ProtagonistController pc(pm);
+    auto pc = std::make_shared<ProtagonistController>(pm);
 
     //healthPacks
-    HealthPackModel hpm(w.getHealthPacks(), 10);
-    HealthPackController hpc(std::make_shared<HealthPackModel>(hpm));
+    auto hpm = std::make_shared<HealthPackModel>(w.getHealthPacks(), 10);
+    auto hpc = std::make_shared<HealthPackController>(hpm);
 
-    //enemies
-    EnemyModel em;
-    em.setEnemyType("Enemy");
-    PEnemyModel pem;
-    pem.setEnemyType("PEnemy");
+    //enemies models
+    auto em = std::make_shared<EnemyModel>();
+    em->setEnemyType("Enemy");
+    auto pem = std::make_shared<PEnemyModel>();
+    pem->setEnemyType("PEnemy");
 
     for (auto& enemy : w.getEnemies()) {
         if (auto pEnemy = dynamic_cast<PEnemy*>(enemy.get())) {
-            pem.addEnemy(std::move(enemy));
+            pem->addEnemy(std::move(enemy));
         } else {
-            em.addEnemy(std::move(enemy));
+            em->addEnemy(std::move(enemy));
         }
     }
 
-//    // Create the enemies views
-//    EnemyViewGraphical evg(mw, std::make_shared<EnemyModel>(em));
-//    PEnemyViewGraphical pevg(mw, std::make_shared<PEnemyModel>(pem));
-
-    // Create an EnemyController and add the enemies models to it
+    //enemy controller
     auto ec = std::make_shared<EnemyController>();
-    /*
-    In order for ec to pass a shared pointer to itself to its PEnemyTimer, we need to do it
-    after the EnemyController has been created. This is because the shared pointer is
-    not initialized until the constructor has finished executing.
-    */ 
     ec->init();
-    ec->addEnemyModel(std::make_shared<EnemyModel>(em));
-    ec->addEnemyModel(std::make_shared<PEnemyModel>(pem));
-//    ec.addEnemyGraphicalView(std::make_shared<EnemyViewGraphical>(evg));
-//    ec.addEnemyGraphicalView(std::make_shared<PEnemyViewGraphical>(pevg));
-    
-    /// TODO: adapt other controllers to be made shared before passed like enemycontroller
-    return std::make_shared<EasyLevel>(std::make_shared<TileController>(tc),
-                                std::make_shared<ProtagonistController>(pc), 
-                                std::make_shared<HealthPackController>(hpc),
-                                ec);
+    ec->addEnemyModel(em);
+    ec->addEnemyModel(pem);
 
-
+    return std::make_shared<EasyLevel>(tc,  pc, hpc, ec);
 }
 
