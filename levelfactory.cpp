2639c45276a3dbb4623afcbe60678a30d5cc9a0d
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2023-12-20 21:56:30 +0100

    Extracted common code from individual factories into abstract

--- levelfactory.cpp
+++ levelfactory.cpp
@@ -4,3 +4,22 @@ LevelFactory::LevelFactory()
 {
 
 }
+
+std::tuple<std::shared_ptr<TileController>, std::shared_ptr<ProtagonistController>, std::shared_ptr<HealthPackController> > LevelFactory::basicControllers(World &w)
+{
+    //tile
+    auto tm = std::make_shared<TileModel>();
+    tm->populateTileMap(w.getRows(), w.getCols(), w.getTiles());
+    auto tc = std::make_shared<TileController>(tm);
+
+    //protagonist:
+    std::unique_ptr<Protagonist> protagonistPtr = w.getProtagonist();
+    auto pm = std::make_shared<ProtagonistModel>(std::move(protagonistPtr));
+    auto pc = std::make_shared<ProtagonistController>(pm);
+
+    //healthPacks
+    auto hpm = std::make_shared<HealthPackModel>(w.getHealthPacks(), 10);
+    auto hpc = std::make_shared<HealthPackController>(hpm);
+
+    return std::make_tuple(tc, pc, hpc);
+}
--- levelfactory.h
+++ levelfactory.h
@@ -1,15 +1,16 @@
 #ifndef LEVELFACTORY_H
 #define LEVELFACTORY_H
 
-
-#include "mainwindow.h"
 #include "level.h"
 
 class LevelFactory
 {
 public:
     LevelFactory();
-    virtual std::shared_ptr<Level> createWorld() {};
+    virtual std::shared_ptr<Level> createWorld() = 0;
+
+protected:
+    std::tuple<std::shared_ptr<TileController>, std::shared_ptr<ProtagonistController>, std::shared_ptr<HealthPackController>> basicControllers(World& w);
 };
 
 #endif // LEVELFACTORY_H
