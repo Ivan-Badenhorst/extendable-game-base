b63db6e9ffbb39329448192fccf56adf69893d29
Sudarshan Iyengar <sudarshan.iyengar@student.kuleuven.be>
2024-01-06 16:03:53 +0100

    📝 Documented level interface and implementing classes

--- include/level/mediumlevel.h
+++ include/level/mediumlevel.h
@@ -5,20 +5,54 @@
 #include "enemycontroller.h"
 #include "tilecontroller.h"
 #include "healthpackcontroller.h"
-
 #include "level.h"
 
+
+/**
+ * @class MediumLevel
+ * @brief Class representing a medium level in the game.
+ *
+ * This class implements Level and provides implementations for the
+ * methods to get controllers for tiles, health packs, the protagonist, and enemies.
+ */
 class MediumLevel: public Level
 {
 public:
+
+    /**
+     * @brief Constructor that initializes the controllers for this MediumLevel.
+     * @param tc Shared pointer to the TileController.
+     * @param pc Shared pointer to the ProtagonistController.
+     * @param hpc Shared pointer to the HealthPackController.
+     * @param ec Shared pointer to the EnemyController.
+     */
     MediumLevel(std::shared_ptr<TileController> tc,
                 std::shared_ptr<ProtagonistController> pc,
                 std::shared_ptr<HealthPackController>hpc,
                 std::shared_ptr<EnemyController> ec);
 
+    /**
+     * @brief Getter for TileController for this level.
+     * @return Shared pointer to the TileController.
+     */
     std::shared_ptr<TileController> getTileController() const override;
+
+    /**
+     * @brief Getter for HealthPackController for this level.
+     * @return Shared pointer to the HealthPackController.
+     */
     std::shared_ptr<HealthPackController> getHpController() const override;
+
+    /**
+     * @brief Get the ProtagonistController for this level.
+     * @return Shared pointer to the ProtagonistController.
+     */
     std::shared_ptr<ProtagonistController> getProtController() const override;
+
+    /**
+     * @brief Get the EnemyController for this level.
+     * @return Shared pointer to the EnemyController.
+     */    
     std::shared_ptr<EnemyController> getEnemyController() const override;
 
 private:
