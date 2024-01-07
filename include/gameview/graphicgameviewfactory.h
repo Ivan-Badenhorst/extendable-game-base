5be2acb8bfe492db0294c7f03359a52f7f2ff6a8
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2024-01-07 13:04:45 +0100

    documentation added to graphicgameviewfactory.h

--- include/gameview/graphicgameviewfactory.h
+++ include/gameview/graphicgameviewfactory.h
@@ -1,11 +1,32 @@
+/**
+ * @file graphicgameviewfactory.h
+ * @brief Defines the GraphicGameViewFactory class.
+ * Responsible for creating graphical game views by inheriting from GameViewFactory.
+ */
+
 #ifndef GRAPHICGAMEVIEWFACTORY_H
 #define GRAPHICGAMEVIEWFACTORY_H
 
 #include "gameviewfactory.h"
+
+/**
+ * @class GraphicGameViewFactory
+ * @brief Responsible for creating graphical game views by inheriting from GameViewFactory.
+ */
 class GraphicGameViewFactory: public GameViewFactory
 {
 public:
+
+    /**
+     * @brief Default constructor for GraphicGameViewFactory.
+     */
     GraphicGameViewFactory();
+
+    /**
+     * @brief Creates a graphical game view.
+     * @param mainWindow Reference to the MainWindow object.
+     * @return A unique pointer to the created game view.
+     */
     std::unique_ptr<GameView> createView(MainWindow& mainWindow) override;
 };
 
